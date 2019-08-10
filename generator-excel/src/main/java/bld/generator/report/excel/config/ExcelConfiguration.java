2a08d95c51ece78474d2e3ebeab8313eb66a72cf
francesco <you@example.com>
2019-08-10 15:55:21 +0200

    config

---
+++ generator-excel/src/main/java/bld/generator/report/excel/config/ExcelConfiguration.java
@@ -0,0 +1,25 @@
+package bld.generator.report.excel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import bld.generator.report.excel.GenerateExcel;
+import bld.generator.report.excel.impl.GenerateExcelImpl;
+import bld.generator.report.utils.ValueProps;
+import bld.generator.report.utils.ValuePropsImpl;
+
+@Configuration
+public class ExcelConfiguration {
+	
+	@Bean
+	public GenerateExcel generatorExcel() {
+		return new GenerateExcelImpl();
+	}
+	
+
+	@Bean
+	public ValueProps valueProps() {
+		return new ValuePropsImpl();
+	}
+	
+}
