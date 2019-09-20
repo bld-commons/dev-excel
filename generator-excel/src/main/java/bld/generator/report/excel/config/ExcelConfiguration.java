428adae88865fe919e2d7bd25240b92b2cf7684b
Francesco <francesco.baldi@dxcfds.com>
2019-09-20 14:28:14 +0200

    configuration

--- generator-excel/src/main/java/bld/generator/report/excel/config/ExcelConfiguration.java
+++ generator-excel/src/main/java/bld/generator/report/excel/config/ExcelConfiguration.java
@@ -1,25 +1,19 @@
 package bld.generator.report.excel.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 import bld.generator.report.excel.GenerateExcel;
-import bld.generator.report.excel.impl.GenerateExcelImpl;
 import bld.generator.report.utils.ValueProps;
-import bld.generator.report.utils.ValuePropsImpl;
 
 @Configuration
 public class ExcelConfiguration {
 	
-	@Bean
-	public GenerateExcel generatorExcel() {
-		return new GenerateExcelImpl();
-	}
+	@Autowired
+	private GenerateExcel generateExcel;
 	
+	@Autowired
+	private ValueProps valueProps;
 
-	@Bean
-	public ValueProps valueProps() {
-		return new ValuePropsImpl();
-	}
 	
 }
