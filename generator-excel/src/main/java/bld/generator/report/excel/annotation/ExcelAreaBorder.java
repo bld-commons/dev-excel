ef813fed7cfd6798eaf78501c5ab95d76a9e503b
Francesco Baldi <fbaldi2@dxc.com>
2020-07-31 03:28:03 +0200

    area-border

--- generator-excel/src/main/java/bld/generator/report/excel/annotation/ExcelAreaBorder.java
+++ generator-excel/src/main/java/bld/generator/report/excel/annotation/ExcelAreaBorder.java
@@ -1,3 +1,8 @@
+/*
+ * @author Francesco Baldi
+ * @mail francesco.baldi1987@gmail.com
+ * @class bld.generator.report.excel.annotation.ExcelAreaBorder.java
+ */
 package bld.generator.report.excel.annotation;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
@@ -5,12 +10,31 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 
+/**
+ * The Interface ExcelAreaBorder.<br>
+ * ExcelAreaBorder is used to set a border on an area of cells.<br>
+ * ExcelAreaBorder is composed by:
+ * <li>
+ * <ul>AreaRange - to define an area of cells</ul>
+ * <ul>Border - to set the border type</ul>
+ * </li>
+ */
 @Documented
 @Retention(RUNTIME)
 public @interface ExcelAreaBorder {
 
+	/**
+	 * Area range.
+	 *
+	 * @return the string
+	 */
 	public String areaRange();
 	
+	/**
+	 * Border.
+	 *
+	 * @return the excel border
+	 */
 	public ExcelBorder border();
 	
 }
