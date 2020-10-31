4bba1255395d073d982ea2af24bd5b1da29e8dad
Francesco Baldi <fbaldi2@dxc.com>
2020-10-31 15:03:25 +0100

    documentation

--- generator-excel/src/main/java/bld/generator/report/excel/annotation/ExcelSubtotals.java
+++ generator-excel/src/main/java/bld/generator/report/excel/annotation/ExcelSubtotals.java
@@ -17,7 +17,7 @@ import java.lang.annotation.Target;
  * ExcelSubtotals and {@link bld.generator.report.excel.annotation.ExcelSubtotal} are used To calculate the subtotals grouped by the value of the first column, 
  * the function type is defined within of {@link bld.generator.report.excel.annotation.ExcelSubtotal}, here instead you set the "startLabel" and "endLabel" properties to be concatenated to the first column and to define the subtotal label that groups all rows.<br>
  * This annotation is defined within classes of type "{@link bld.generator.report.excel.RowSheet}".<br>
- * The property ExcelCellLayout define the cell layout.
+ * The property ExcelCellLayout define the cell layout instead the property sumForGroup enable the subtotals grouped by the value of the first column.<br>
  */
 @Documented
 @Retention(RUNTIME)
