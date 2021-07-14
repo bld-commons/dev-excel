0a593d14680bf0978e1a316ae467539e722efd6f
Francesco Baldi <francesco.baldi1987@gmail.com>
2021-07-14 18:21:34 +0200

    comments

--- read-excel/src/main/java/bld/read/report/csv/ReadCsv.java
+++ read-excel/src/main/java/bld/read/report/csv/ReadCsv.java
@@ -16,10 +16,29 @@ public interface ReadCsv {
 
 
 	
+	/**
+	 * Convert csv to entity.
+	 *
+	 * @param <T>     the generic type
+	 * @param csvRead the csv read
+	 * @param classT  the class T
+	 * @return the list
+	 * @throws Exception the exception
+	 */
 	public abstract <T extends RowSheetRead> List<T>  convertCsvToEntity(CsvRead<T> csvRead,Class<T> classT) throws Exception;
 
 	
 	
+	/**
+	 * Convert csv to entity.
+	 *
+	 * @param <T>      the generic type
+	 * @param csvRead  the csv read
+	 * @param pathFile the path file
+	 * @param classT   the class T
+	 * @return the list
+	 * @throws Exception the exception
+	 */
 	public abstract <T extends RowSheetRead> List<T> convertCsvToEntity(CsvRead<T> csvRead, String pathFile,Class<T> classT) throws Exception;
 	
 }
