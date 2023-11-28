f1e361b991997cb1d813eadcb31828d7ed75a4a4
Francesco Baldi <francesco.baldi1987@gmail.com>
2023-11-28 11:53:14 +0100

    number

--- generator-excel/src/main/java/bld/generator/report/excel/annotation/ExcelNumberFormat.java
+++ generator-excel/src/main/java/bld/generator/report/excel/annotation/ExcelNumberFormat.java
@@ -1,10 +1,13 @@
 package bld.generator.report.excel.annotation;
 
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
 
 @Retention(RUNTIME)
+@Target({FIELD})
 public @interface ExcelNumberFormat {
 
 	public String value() default "";
