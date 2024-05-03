d4647403be2b4eaa4131d5e729fa114ae8130a93
Francesco Baldi <francesco.baldi1987@gmail.com>
2024-05-04 00:18:14 +0200

    comment

--- common-spreadsheet/src/main/java/bld/common/spreadsheet/exception/SpreadsheetException.java
+++ common-spreadsheet/src/main/java/bld/common/spreadsheet/exception/SpreadsheetException.java
@@ -1,24 +1,58 @@
+/*
+ * @auth Francesco Baldi
+ * @class bld.common.spreadsheet.exception.SpreadsheetException.java
+ */
 package bld.common.spreadsheet.exception;
 
+/**
+ * The Class SpreadsheetException.
+ */
 @SuppressWarnings("serial")
 public class SpreadsheetException extends RuntimeException {
 
+	/**
+	 * Instantiates a new spreadsheet exception.
+	 */
 	public SpreadsheetException() {
 		super();
 	}
 
+	/**
+	 * Instantiates a new spreadsheet exception.
+	 *
+	 * @param message the message
+	 * @param cause the cause
+	 * @param enableSuppression the enable suppression
+	 * @param writableStackTrace the writable stack trace
+	 */
 	public SpreadsheetException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
 	}
 
+	/**
+	 * Instantiates a new spreadsheet exception.
+	 *
+	 * @param message the message
+	 * @param cause the cause
+	 */
 	public SpreadsheetException(String message, Throwable cause) {
 		super(message, cause);
 	}
 
+	/**
+	 * Instantiates a new spreadsheet exception.
+	 *
+	 * @param message the message
+	 */
 	public SpreadsheetException(String message) {
 		super(message);
 	}
 
+	/**
+	 * Instantiates a new spreadsheet exception.
+	 *
+	 * @param cause the cause
+	 */
 	public SpreadsheetException(Throwable cause) {
 		super(cause);
 	}
